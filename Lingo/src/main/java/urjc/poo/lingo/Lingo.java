26810f81b42438e46c51fda30e51702f48bba9f1
Piwccle <sergiosergi11@hotmail.com>
2022-12-28 13:23:14 +0100

    manolo

--- Lingo/src/main/java/urjc/poo/lingo/Lingo.java
+++ Lingo/src/main/java/urjc/poo/lingo/Lingo.java
@@ -1,14 +1,11 @@
 
 package urjc.poo.lingo;
 
-/**
- *
- * @author manuelalejandrojimenezfernandez
- */
+
 public class Lingo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello !");
+        System.out.println("Hello Manolazo!");
     }
 }
