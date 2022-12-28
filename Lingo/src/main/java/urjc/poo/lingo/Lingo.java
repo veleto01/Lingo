cdfae5eddeaae4272fb1ac28de866e4d2538cf68
Manuel Alejandro <31254691+veleto01@users.noreply.github.com>
2022-12-28 13:17:48 +0100

    update
    he cambiado esto

--- Lingo/src/main/java/urjc/poo/lingo/Lingo.java
+++ Lingo/src/main/java/urjc/poo/lingo/Lingo.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
 
 package urjc.poo.lingo;
 
@@ -13,5 +9,6 @@ public class Lingo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello !");
     }
 }
